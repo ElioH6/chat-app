b975d08abb8b75f41c21780bcd8f124ff1b771f1
ElioH6 <145810382+ElioH6@users.noreply.github.com>
2025-02-12 21:06:05 +0200

    small fix

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'http://localhost:5000',
         credentials: true,
     }
 ));
